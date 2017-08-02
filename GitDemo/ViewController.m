c1b7f1195508c23bb84c819727c3ef4bb13d246e
范超 <1446486761@qq.com>
2017-08-02 17:44:10 +0800

    first commit

---
+++ GitDemo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by yitudev12 on 2017/8/2.
+//  Copyright © 2017年 yitu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    // first commit
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ GitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDemo
+//
+//  Created by yitudev12 on 2017/8/2.
+//  Copyright © 2017年 yitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
